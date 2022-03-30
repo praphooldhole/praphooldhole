18a76c05577c3ad77b8aaa6dca1465acb3ade305
praphs <praphooldhole@gmail.com>
2022-03-31 00:12:31 +0530

    newsillyscript

---
+++ sillyscript.ps1
@@ -0,0 +1,5 @@
+##powershell
+#invoke a web page for example google.com or cnbc.com using start-process cmdlet
+#msdocs:https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process?view=powershell-7.2 
+Start-Process "https:\\google.com" | Start-Process "https:\\cnbc.com"
+#end
